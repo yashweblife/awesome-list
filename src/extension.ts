e3c5e06ac44d0d9ede67b5ea8cc596071df9dc66
yashweblife <yashweblife20@gmail.com>
2023-06-15 20:42:02 -0500

    working code 👍

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,23 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { readFileSync } from 'fs';
+import { join } from "path";
 import * as vscode from 'vscode';
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "awesome-list" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('awesome-list.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from &#34;Awesome List&#34;!');
-	});
-
-	context.subscriptions.push(disposable);
+function getWebViewContent(p: string) {
+	const resPath = vscode.Uri.file(
+		join(__dirname, p)
+	)
+	const file = readFileSync(resPath.fsPath, 'utf-8');
+	return file;
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function activate(context: vscode.ExtensionContext) {
+	console.log("Hello World");
+	const disposable = vscode.commands.registerCommand("awesome-list.open", function () {
+		const panel = vscode.window.createWebviewPanel("awesomeList", "Awesome List", vscode.ViewColumn.Beside, {
+			enableScripts:true
+		});
+		panel.webview.html = getWebViewContent("./index.html");
+		vscode.window.showInformationMessage("Hello World");
+	});
+	context.subscriptions.push(disposable);
+}
+export function deactivate() { }
